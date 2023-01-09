5c8a0df661f1a8ff976105e5bb0c53a5bcfc936f
mknyambura <kariuki.mercie@gmail.com>
2023-01-09 10:37:23 +0300

    update commit

--- resto.js
+++ resto.js
@@ -6,9 +6,27 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec => {
+        const top = window.scrollY;
+        const height= sec.offsetHeight;
+        const offset = sec.offsetTop - 150;
+        const id = sec.getAttribute('id');
+
+        if(top => offset && top < offset + height ){
+            navLinks.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+            });
+        };
+    });
 }
 document.querySelector('#search').onclick = () => {
     document.querySelector('#search-form').classList.toggle('active');
@@ -17,24 +35,10 @@ document.querySelector('#search-close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
 }
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
+function loader(){
+    document.querySelector('.loader').classList.add('fade-out');
+}
+function fadeOut(){
+    setInterval(loader, 3000);
+}
+window.onload = fadeOut;
